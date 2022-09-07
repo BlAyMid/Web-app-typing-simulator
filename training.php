94d52a731d5b3f29686436b1fa6aff53c1fdafcc
Egor <blaymid@gmail.com>
2022-09-07 11:42:10 +0300

    Updated trainer on BlAyMid version. Browser trainer v.1. Add: 3 files. The version is working and tested.
    Plans:
    Develop achievements;
    Add a working version to the site;
    Adaptive layout for mobile and tablets;
    Add a theme selection on the page;

---
+++ training.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width">
+    <title id="Egor">Trainer</title>
+    <link rel="stylesheet" href="/style.css" type="text/css">
+</head>
+<body>
+
+<header>
+
+</header>
+
+<main>
+    <div id="parent">
+        <h1 id="start">Press Enter</h1>
+        <div id="training">
+            <div id="text_block">
+                <div id="time_css">
+                    <p id="time">Отсчёт:</p>
+                    <p id="time_value"></p>
+                </div>
+
+                <div id="progress_css">
+                    <p id="progress">Точность: </p>
+                    <p id="progress_value">0</p>
+                </div>
+
+                <div id="errors_css">
+                    <p id="errors">Ошибки: </p>
+                    <p id="errors_value">0</p>
+                </div>
+            </div>
+
+            <div>
+                <div id="game">
+                    <p id="text_text"></p>
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
+
+<script src="/main.js"></script>
+</body>
+
+<footer>
+</footer>
+
+</html>
